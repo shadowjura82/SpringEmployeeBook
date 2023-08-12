dd4677b4168032d9033a81a7584fce6797e4e340
Yurii <shadowjura82@gmail.com>
2023-08-12 17:48:34 +0200

    Initial commit

---
+++ src/main/java/skypro/employee/SpringEmployeeBook/SpringEmployeeBookApplication.java
@@ -0,0 +1,14 @@
+package skypro.employee.SpringEmployeeBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringEmployeeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeBookApplication.class, args);
+	}
+
+}
